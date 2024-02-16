082a8d3d03880e6921c61b95037074181f10d3df
yassin@siouda.com <yassin@siouda.com>
2024-02-16 12:33:43 +0100

    feat(templates): adding pythonxflask

---
+++ templates/python_flask/src/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+import random
+API_PASSWORD = os.environ.get('API_PASSWORD')
+API_USERNAME = os.environ.get('API_USERNAME')
+
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    if username == API_USERNAME and password == API_PASSWORD:
+        return username
+
+@app.route("/")
+@auth.login_required
+def hello_world():
+    return "🚀 App is running"
+
+
+# APi example route returning pet param from query string
+@app.route("/api/v1/myPet/<name>", methods=['GET'])
+@auth.login_required
+def mypet(name):
+
+    emojis = ["🐶","🐱","🐭","🐹","🐰","🦊","🐻","🐼","🐨","🐯","🦁","🐮","🐷","🐸","🐵","🐔","🐧"]
+    random_emoji = random.choice(emojis)
+
+    return f"{random_emoji} {name}"
